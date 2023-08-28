d097f8f5334b9f7837786581b0da25c58c30705b
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-28 22:17:51 +0800

    Update name_prediction.py

--- name_prediction.py
+++ name_prediction.py
@@ -1,22 +1,22 @@
 import streamlit as st
-import nltk
+import pickle
 
-# Load the trained NaiveBayesClassifier from the .pkl file
+# Load the trained model from the .pkl file
 def load_model(model_path):
     with open(model_path, 'rb') as file:
-        model = nltk.NaiveBayesClassifier.load(file)
+        model = pickle.load(file)
     return model
 
 def predict_gender(name, model):
-    features = extract_gender_features(name)  # Assuming you have the same feature extraction method
-    return model.classify(features)
+    gender = model.predict([name])[0]
+    return gender
 
 def main():
     st.title("Gender Prediction App")
     st.write("Enter a name to predict the gender:")
 
     # Load the model
-    model_path = 'name_prediction.pkl'  # Update with the actual path
+    model_path = 'path_to_your_model.pkl'  # Update with the actual path
     model = load_model(model_path)
 
     name_input = st.text_input("Name:")
